8f0e1ad5376e7874b5c1f9361f49dffc2ad16922
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-11-14 16:26:58 -0200

    Criação do método GET: GetAllProducts

--- IntegritasChallenge/Integritas/Controllers/ProductController.cs
+++ IntegritasChallenge/Integritas/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Web.Http;
 using Integritas.Models;
 
 namespace Integritas.Controllers
@@ -7,6 +8,14 @@ namespace Integritas.Controllers
     public class ProductController : ApiController
     {
         private readonly ProductDbContext _db = new ProductDbContext();
+
+        /* Método responsável por resgatar todos os Produtos do Banco */
+        [HttpGet]
+        [Route("products")]
+        public IQueryable<Product> GetAllProducts()
+        {
+            return _db.Products;
+        } 
 
         /* Aqui estarei desconectando a conexão com a base de dados */
         protected override void Dispose(bool disposing)
