eb62ac40be2a672bc1750970141ed9edccd657d0
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-11-14 14:46:46 -0200

    Criação de uma classe modelo: ProductDbContext.class

---
+++ IntegritasChallenge/Integritas/Models/ProductDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Integritas.Models
+{
+    public class ProductDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            /* Aqui estou removendo a pluralização da Tabela para evitar erros. Assim encontrará
+             * a tabela correta: Product */
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
