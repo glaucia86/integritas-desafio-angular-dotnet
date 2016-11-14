6d98c1af23adde34ba3b6996f7a5bbe3a0cde578
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-11-14 14:36:08 -0200

    Criação da classe Modelo: Product.class

---
+++ IntegritasChallenge/Integritas/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Integritas.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductType { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
