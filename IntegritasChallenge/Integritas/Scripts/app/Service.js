285ce2db74b22816b0bfcc37c2ff361a57db1233
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-11-14 17:05:40 -0200

    Desenvolvimento do lado Service.js para consumir os método criados no Controller WebApi

--- IntegritasChallenge/Integritas/Scripts/app/Service.js
+++ IntegritasChallenge/Integritas/Scripts/app/Service.js
@@ -1,3 +1,17 @@
-﻿app.service("integritasService", function ($rootScope, $http) {
+﻿/**
+ * Arquivo: Service.js
+ * Author: Glaucia Lemos
+ * Data: 11/14/2016
+ * Description: Arquivo responsável por consumir os métodos criados no Controller - WebApi 
+ *  no lado do server-side através do Angular.Js
+ */
 
+app.service("integritasService", function ($rootScope, $http) {
+
+    /*
+     * Chamada via AJAX através do Angular.Js do método: GetAllProducts
+     */
+    this.GetAllProducts = function() {
+        return $http.get("/api/v1/public/products");
+    }
 });
