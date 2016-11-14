61d000a67256b67243559d1f6741e280805f37ed
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-11-14 16:14:25 -0200

    Criação de um Controller para codificar a lógica da app.

---
+++ IntegritasChallenge/Integritas/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using Integritas.Models;
+
+namespace Integritas.Controllers
+{
+    [RoutePrefix("api/v1/public")]
+    public class ProductController : ApiController
+    {
+        private readonly ProductDbContext _db = new ProductDbContext();
+
+        /* Aqui estarei desconectando a conexão com a base de dados */
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
